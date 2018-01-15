5c9bcb452a6a478b6312939d26eb88b238ddbfaf
Jsmith2021 <league@WTS5.attlocal.net>
2018-01-14 17:35:44 -0800

    RobotHouse

---
+++ src/HouseMaker.java
@@ -0,0 +1,70 @@
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class HouseMaker {
+	Robot r = new Robot();
+	
+public void leftCorner() {
+	r.moveTo(60, 860);
+}
+public void makeHouse(int height) {
+	r.penDown();
+	r.setSpeed(10);
+	r.move(height);
+	r.turn(90);
+	if(height == 250) {
+	drawPointyRoof();
+	}
+	else {
+		drawFlatRoof();
+	}
+	r.move(height);
+	r.turn(-90);
+	r.move(50);
+	r.turn(-90);
+	
+}
+public void makeHouse(String height) {
+	if(height.equals("small")) {
+		makeHouse(60);
+		
+	}
+	else if(height.equals("medium")) {
+		makeHouse(120);
+	}
+	else if(height.equals("large")) {
+		makeHouse(250);
+	}
+	
+}
+public void makeHouseColor(String color) {
+	if(color.equals("red")) {
+		r.setPenColor(Color.red);
+		makeHouse("small");
+	}
+	else if(color.equals("blue")) {
+		r.setPenColor(Color.blue);
+		makeHouse("medium");
+	}
+	else if(color.equals("yellow")){
+			r.setPenColor(Color.yellow);
+			makeHouse("large");
+	}	
+}
+
+public void drawPointyRoof() {
+	r.turn(-45);
+	r.move(25);
+	r.turn(90);
+	r.move(25);
+	r.turn(45);
+	
+}
+public void drawFlatRoof() {
+	
+	r.move(50);
+	r.turn(90);
+	
+}
+}
---
+++ src/RobotHouses.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class RobotHouses {
+public static void main(String[] args) {
+	HouseMaker house = new HouseMaker();
+	house.leftCorner();
+	for(int i = 0; i < 3; i ++) {
+	house.makeHouseColor("red");
+	house.makeHouseColor("yellow");
+	house.makeHouseColor("blue");
+	}
+}
+}
